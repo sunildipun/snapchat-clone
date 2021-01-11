049273c5b42bc386598f184358604dea594ae501
sunildipun <sunil.6318ie@gmail.com>
2021-01-11 20:17:09 +0530

    header for chats with css

--- src/component/Chats/Chats.component.jsx
+++ src/component/Chats/Chats.component.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
-import './Chats.style.css'
+import './Chats.style.css';
+
+import  { Avatar } from '@material-ui/core';
+
+import SearchIcon from "@material-ui/icons/Search";
+import ChatBubbleIcon from "@material-ui/icons/ChatBubble";
+
 
 const Chats = () => {
     return (
         <div className="chats">
-            Here in chats
+            <div className="chats__header">
+                <Avatar className="chats__avatar" />
+                <div className="chats__search">
+                    <SearchIcon />
+                    <input placeholder="Friends" type="text" />
+                </div>
+                <ChatBubbleIcon className="chats_chatIcon"/>
+            </div>
         </div>
     )
 }
